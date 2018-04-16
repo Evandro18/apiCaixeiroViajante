430836b7a19aad807d5d882e878f4c1eb2bb7a6f
Evandro Terra <evandro18.goncalves@gmail.com>
2018-04-16 11:10:31 -0400

    init

---
+++ express.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+const fs = require('fs');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const obj = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+app.get('/caxeiro', function (req, res) {
+  res.json(obj);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
